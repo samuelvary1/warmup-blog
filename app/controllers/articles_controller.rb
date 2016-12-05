0219bec3eab9d890c2b14f3f2822233d199e0717
Sam Vary <sam.vary@gmail.com>
2016-12-05 13:33:15 -0500

    finished the basic tutorial

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+	http_basic_authenticate_with name: "sam.vary", password: "secret", except: [:index, :show]
+	
 	def index
 		@articles = Article.all
 	end
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,16 @@
 class CommentsController < ApplicationController
+	http_basic_authenticate_with name: "sam.vary", password: "secret", except: [:index, :show]
+
 	def create
 		@article = Article.find(params[:article_id])
 		@comment = @article.comments.create(comment_params)
+		redirect_to article_path(@article)
+	end
+
+	def destroy
+		@article = Article.find(params[:article_id])
+		@comment = @article.comments.find(params[:id])
+		@comment.destroy
 		redirect_to article_path(@article)
 	end
 
